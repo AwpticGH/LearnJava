a1e1dda7306567491a2ca50ff515ce35ffeaabed
AwpticWindows <rafihariyadi01@gmail.com>
2022-08-02 09:13:06 +0700

    Product Maximizer Done

--- src/com/challenges/productMaximizer/Main.java
+++ src/com/challenges/productMaximizer/Main.java
@@ -1,29 +1,55 @@
 package com.challenges.productMaximizer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int target;
+        int total;
+        int result;
         ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> results = new ArrayList<>();
 
-        System.out.print("Input Target : ");
-        target = sc.nextInt();
+        System.out.print("Input Total Number : ");
+        total = sc.nextInt();
 
-        for (int i = 0; i < 3; i++) {
+        System.out.println("Loops : " + loopCounter(total));
+
+        for (int i = 0; i < total; i++) {
             System.out.print("Input Number : ");
             numbers.add(sc.nextInt());
         }
-        countProduct(target, numbers);
 
+        for (int number1 : numbers) {
+            for (int number2 : numbers) {
+                if (number1 != number2 & !results.contains(number1 + number2)) {
+                    System.out.println(number1 + " + " + number2 + " = " + (int)(number1+number2));
+                    results.add(number1 + number2);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(results.toArray()));
+
+        result = results.get(0);
+
+        for (int i = 1; i < results.size(); i++) {
+            if (results.get(i) > result) {
+                result = results.get(i);
+            }
+        }
+
+        System.out.println("Max Product : " + result);
     }
 
-    private static void countProduct(int target, ArrayList<Integer> numbers) {
-        if (target == 0) ;
-
+    private static int loopCounter(int n) throws StackOverflowError {
+        if (n == 0) {
+            return 0;
+        }
+        return loopCounter(n-1) + (n - 1);
     }
 
 }
